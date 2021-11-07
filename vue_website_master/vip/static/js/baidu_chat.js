957901bf4946c1ab957c16b3159c4da508ec07f4
shan <1033944566@qq.com>
2021-11-07 22:57:18 +0800

    参照网站

---
+++ vue_website_master/vip/static/js/baidu_chat.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: toloy
+ * @Date: 2021-02-10 14:36:23
+ * @LastEditTime: 2021-03-05 13:13:55
+ */
+// var _hmt = _hmt || [];
+// (function() {
+//   var hm = document.createElement("script");
+//   hm.src = "https://hm.baidu.com/hm.js?fc7c0315dcca0a5219b6c077d9d71cf7";
+//   var s = document.getElementsByTagName("script")[0]; 
+//   s.parentNode.insertBefore(hm, s);
+// })();
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fc7c0315dcca0a5219b6c077d9d71cf7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
