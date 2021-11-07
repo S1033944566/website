bac97e0f2ed84044960da72aae4c09d6a35642fd
shan <1033944566@qq.com>
2021-11-07 22:52:50 +0800

    更新文档

--- java_website_manager/src/easyuser.sql
+++ java_website_manager/src/easyuser.sql
@@ -1,23 +1,8 @@
-/*
-Navicat MySQL Data Transfer
 
-Source Server         : Testblog
-Source Server Version : 50556
-Source Host           : localhost:3306
-Source Database       : easyproject
-
-Target Server Type    : MYSQL
-Target Server Version : 50556
-File Encoding         : 65001
-
-Date: 2020-05-27 14:48:38
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for easyuser
--- ----------------------------
+
 DROP TABLE IF EXISTS `easyuser`;
 CREATE TABLE `easyuser` (
   `id` int(24) NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -29,9 +14,7 @@ CREATE TABLE `easyuser` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of easyuser
--- ----------------------------
+
 INSERT INTO `easyuser` VALUES ('1', 'admin', '123456', '123@qq.com', '超级管理员', '1');
 INSERT INTO `easyuser` VALUES ('2', 'naughty', '888888', '456@qq.com', '普通管理员', '0');
 INSERT INTO `easyuser` VALUES ('3', 'flysky01', '987654', 'qwe@qq.com', '普通用户', '1');
--- java_website_manager/src/submenu.sql
+++ java_website_manager/src/submenu.sql
@@ -1,23 +1,6 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : Testblog
-Source Server Version : 50556
-Source Host           : localhost:3306
-Source Database       : easyproject
-
-Target Server Type    : MYSQL
-Target Server Version : 50556
-File Encoding         : 65001
-
-Date: 2020-05-27 14:48:58
-*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for submenu
--- ----------------------------
 DROP TABLE IF EXISTS `submenu`;
 CREATE TABLE `submenu` (
   `id` int(50) NOT NULL AUTO_INCREMENT,
@@ -29,9 +12,6 @@ CREATE TABLE `submenu` (
   CONSTRAINT `submenu_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `mainmenu` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=204 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of submenu
--- ----------------------------
 INSERT INTO `submenu` VALUES ('101', '用户列表', '/user', '100');
 INSERT INTO `submenu` VALUES ('102', '修改权限', '/rights', '100');
 INSERT INTO `submenu` VALUES ('103', ' 网页模块', '/sport', '100');
