7ca6616bf7d18f1a6e4bb8c210ea9c11abb8ba6d
shan <1033944566@qq.com>
2021-11-10 10:39:02 +0800

    页面优化

---
+++ vue_website_master/src/components/js/message.js
@@ -0,0 +1,342 @@
+ window.__NUXT__ = function (e, t, i, s, a, n, c, m, x, y, h, g, k, p, d, w, o, b) {
+    return t[0] = "400-116-7788", i[0] = {wikiTypeId: 1, wikiType: s, sort: 1, isDisabled: 0}, i[1] = {
+    wikiTypeId: 2,
+    wikiType: "家政保姆",
+    sort: 2,
+    isDisabled: 0
+}, i[2] = {wikiTypeId: 3, wikiType: "月子知识", sort: 3, isDisabled: 0}, i[3] = {
+    wikiTypeId: 4,
+    wikiType: "老年护理",
+    sort: 4,
+    isDisabled: 0
+}, i[4] = {wikiTypeId: 5, wikiType: a, sort: 5, isDisabled: 0}, i[5] = {
+    wikiTypeId: 6,
+    wikiType: "蓝族新闻",
+    sort: 6,
+    isDisabled: 0
+}, n[0] = c, m[0] = c, x[0] = "http://api.dfenqi.com/file/sys/100/kf2.jpeg", y[0] = "http://api.dfenqi.com/file/sys/100/banner/sy.jpg", h[0] = ["母婴展咨询网", "http://www.51muyingzhan.com/"], h[1] = ["和睦家医疗", "https://shanghai.ufh.com.cn/"], h[2] = ["儿童教育网", "http://www.xychild.com/"], h[3] = ["华夏家博会", "http://www.jiabohui021.com/"], h[4] = ["天鹅到家", "https://www.daojia.com"], g[0] = {
+    wikiType: s,
+    wikiList: [{
+    wikiImage: "http://api.dfenqi.com/file/image/2021-04-12/100/wiki/da5b5929-cd9f-4edd-8890-abc7c2084b63.jpg",
+    wikiId: 340,
+    wikiTitle: "住家保姆初次入户该怎么做？"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-04-01/100/wiki/8cb31666-cb5d-41ba-997f-9d8a923b4f56.jpeg",
+    wikiId: 274,
+    wikiTitle: "喂奶的这些秘诀，你知道吗？"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-31/100/wiki/3ca922e4-7598-44b3-b31e-3c108a0c7e7b.png",
+    wikiId: 271,
+    wikiTitle: "婴儿油是不是宝宝必需品"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-30/100/wiki/49d4adad-896e-4fdc-aeb3-1749cbfe8670.png",
+    wikiId: 265,
+    wikiTitle: "大人和宝宝的衣服有必要分开洗吗"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-29/100/wiki/d80e4946-375d-470d-87b3-e443548e2895.png",
+    wikiId: 259,
+    wikiTitle: "母乳喂养到多大最好"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-29/100/wiki/2e4d142d-fd01-485f-a472-c71be06c6b79.png",
+    wikiId: 250,
+    wikiTitle: "嚼碎的食物喂给宝宝真的好吗"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-27/100/wiki/319e38a9-5b65-49ee-8a88-24e54122ef17.png",
+    wikiId: 247,
+    wikiTitle: "宝宝能不能吃蜂蜜"
+}]
+}, g[1] = {
+    wikiType: a,
+    wikiList: [{
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-16/100/wiki/f9fbf580-c7bf-4cd4-a77b-0d1125c45d25.jpg",
+    wikiId: 175,
+    wikiTitle: "在美国怎么请保姆？"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-14/100/wiki/6cc37810-3a70-46a4-88e5-c374a9dbb549.jpg",
+    wikiId: 163,
+    wikiTitle: "找育儿嫂独立带娃，放心吗？"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-13/100/wiki/b3abcf55-9223-498d-b99c-1f084cd896b8.png",
+    wikiId: 154,
+    wikiTitle: "保姆有哪些分类？一个月多少钱？"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-11/100/wiki/fe429d17-d9b5-48c0-b0dc-7bef40a0c78b.jpg",
+    wikiId: 136,
+    wikiTitle: "当月嫂有多难？"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-11/100/wiki/f6655390-3404-49e8-b9e2-cbd52cbf90ef.png",
+    wikiId: 133,
+    wikiTitle: "怎么找一个好的育儿嫂？"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-11/100/wiki/784a5880-d43c-4a7f-b910-7c8815797ca8.jpg",
+    wikiId: 130,
+    wikiTitle: "在上海如何找到靠谱的月嫂？"
+}, {
+    wikiImage: "http://api.dfenqi.com/file/image/2021-03-10/100/wiki/709dcd3b-88c7-49c5-8451-6507df888347.jpg",
+    wikiId: 127,
+    wikiTitle: "现在月嫂的价格为什么这么高？"
+}]
+}, {
+    layout: "default",
+    data: [{tels: t, wikiType: i, mobile: n, weixinNo: m, weixinCode: x, banner: y, link: h, lessWiki: g}],
+    fetch: [],
+    error: null,
+    state: {
+    tels: t,
+    mobile: n,
+    weixinNo: m,
+    weixinCode: x,
+    banner: y,
+    link: h,
+    serverTypeList: [{serverName: "育婴师", description: "0-3岁科学育儿专家", href: "yuyingshi"}, {
+    serverName: "金牌月嫂",
+    description: "24小时母婴精心护理",
+    href: "yuesao"
+}, {serverName: k, description: "全天在家周到服务", href: "baomu"}, {
+    serverName: "儿童早教",
+    description: "4-7岁 黄金期潜力激活",
+    href: "zaojiao"
+}],
+    wikiType: i,
+    lessWiki: g,
+    hasShow: void 0,
+    config: {
+    trainData: [{
+    title: p,
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/gjyys@2x.png"
+}, {
+    title: d,
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/mszj@2x.png"
+}, {
+    title: "母婴护理",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/myhl@2x.png"
+}, {
+    title: w,
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/xetn@2x.png"
+}, {
+    title: "产后康复",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/chkh@2x.png"
+}, {title: o, image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/crspx@2x.png"}, {
+    title: b,
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/jzjspx@2x.png"
+}, {title: "管家培训", image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/gjpx@2x.png"}],
+    promiseData: [{xh: "01", title: "先培训后上岗", text: "每个阿姨都经过专业的技能培训、筛选，放心可靠。"}, {
+    xh: "02",
+    title: "免费更换阿姨",
+    text: "一年内免费更换阿姨"
+}, {xh: "03", title: "100%背景调查", text: "公安联网，确保阿姨无犯罪记录、无征信问题。"}, {
+    xh: "04",
+    title: "100%健康体检",
+    text: "正规医院体检，确保家人无健康风险。"
+}],
+    safeData: [{
+    icon: "http://cdn.xhome.net.cn/homecc-web/assets/images/bjdc@2x.png",
+    width: e,
+    height: e,
+    title: "背景调查",
+    items: [{key: 1, text: "身份信息是否真实"}, {key: 2, text: "有无社会不良记录"}, {key: 3, text: "有无法院诉讼"}]
+}, {
+    icon: "http://cdn.xhome.net.cn/homecc-web/assets/images/jktj@2x.png",
+    title: "健康体检",
+    width: e,
+    height: 55,
+    items: [{key: 1, text: "有无传染疾病"}, {key: 2, text: "有无重大疾病隐患"}, {key: 3, text: "妇科、HV、结核"}, {
+    key: 4,
+    text: "7日内核酸检测"
+}]
+}, {
+    icon: "http://cdn.xhome.net.cn/homecc-web/assets/images/jzbx@2x.png",
+    title: "家政保险",
+    width: 50,
+    height: e,
+    items: [{key: 1, text: "雇主家庭人身及财产"}, {key: 2, text: "人员身故、意外伤害"}, {
+    key: 3,
+    text: "公共交通、病假误工费"
+}, {key: 4, text: "法律费用"}]
+}, {
+    icon: "http://cdn.xhome.net.cn/homecc-web/assets/images/cxda@2x.png",
+    title: "诚信档案",
+    width: 56,
+    height: e,
+    items: [{key: 1, text: "安全真实家人担保"}]
+}, {
+    icon: "http://cdn.xhome.net.cn/homecc-web/assets/images/zypx@2x.png",
+    title: "专业培训持证上岗",
+    width: e,
+    height: 58,
+    items: [{key: 1, text: "专业技能培训"}, {key: 2, text: "岗前培训"}, {key: 3, text: "服务意识培训"}]
+}, {
+    icon: "http://cdn.xhome.net.cn/homecc-web/assets/images/khhp@2x.png",
+    title: "客户好评",
+    width: e,
+    height: e,
+    items: [{key: 1, text: "老客户推荐"}, {key: 2, text: "老阿姨介绍"}, {key: 3, text: "平台金牌阿姨"}]
+}, {
+    icon: "http://cdn.xhome.net.cn/homecc-web/assets/images/xsg@2x.png",
+    title: "先试工再签约",
+    width: 56,
+    height: e,
+    items: [{key: 1, text: "专业技能测试"}, {key: 2, text: "服务态度测试"}, {key: 3, text: "无条件调换"}]
+}, {
+    icon: "http://cdn.xhome.net.cn/homecc-web/assets/images/zytd@2x.png",
+    title: "专业团队",
+    width: e,
+    height: 49,
+    items: [{key: 1, text: "产前护理专家"}, {key: 2, text: "母婴护理专家"}, {key: 3, text: "早教启蒙名师"}, {
+    key: 4,
+    text: "家务师团队"
+}]
+}],
+    formServerTypeData: [{text: "月嫂", id: 1}, {text: "育婴师", id: 2}, {text: k, id: 3}, {
+    text: "早期教育",
+    id: 4
+}, {text: "日常家务", id: 5}, {text: "小时工", id: 6}],
+    serverYYSData: [{
+    key: "yys_1",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/yys/kxwy@2x.png",
+    desc: [{key: "yys_xx_1", text: "能够进行母乳喂养指导"}, {key: "yys_xx_2", text: "合理进行人工喂养"}, {
+    key: "yys_xx_3",
+    text: "能够正确冲调奶粉"
+}, {key: "yys_xx_4", text: "正确准备用具和喂哺婴儿"}, {key: "yys_xx_5", text: "不同月龄婴儿的喂水方式"}, {
+    key: "yys_xx_6",
+    text: "不同月龄的奶量和辅食添加"
+}, {key: "yys_xx_7", text: "正确选择、搭配、固体食物"}, {key: "yys_xx_8", text: "合理营养配餐良好的饮食习惯"}]
+}, {
+    key: "yys_2",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/yys/shzl@2x.png",
+    desc: [{key: "yys_xx_11", text: "为婴儿制造适合睡眠的环境，不同月龄婴儿的睡眠时间"}, {
+    key: "yys_xx_12",
+    text: "正常二便的规律、特性"
+}, {key: "yys_xx_13", text: "三浴锻炼的方法和好处"}, {
+    key: "yys_xx_14",
+    text: "正确包裹婴儿、穿脱衣服、更换尿布，并给予皮肤清洁护理"
+}, {key: "yys_xx_15", text: "定期对婴儿卧具、餐具、玩具、家具进行消毒"}, {
+    key: "yys_xx_16",
+    text: "对婴儿进行眼、外耳道、口腔、腋窝、外阴的清洁"
+}, {key: "yys_xx_17", text: "为婴儿洗澡抚触，做主被动操"}]
+}, {
+    key: "yys_3",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/yys/rchl@2x.png",
+    desc: [{key: "yys_xx_21", text: "生长监测每天的生长记录"}, {
+    key: "yys_xx_22",
+    text: "量具选择和使用的知识"
+}, {key: "yys_xx_23", text: "预防接种"}, {key: "yys_xx_24", text: "常见疾病的预防和护理"}, {
+    key: "yys_xx_25",
+    text: "喂药及简易护理的方法"
+}, {key: "yys_xx_26", text: "预防铅中毒了解铅的来源以及铅进入人体的途径"}, {
+    key: "yys_xx_27",
+    text: "危险因素识别"
+}, {key: "yys_xx_28", text: "常见意外伤害的原因及预防"}]
+}, {
+    key: "yys_4",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/yys/zqjy@2x.png",
+    desc: [{key: "yys_xx_31", text: "大动作的训练方法包括抬头、身、坐、爬，走、跑，跳"}, {
+    key: "yys_xx_32",
+    text: "精细动作的训练方法包括手、眼脑协调"
+}, {key: "yys_xx_33", text: "游戏陪伴包括儿歌、舞蹈、英语教育、发展测评"}, {
+    key: "yys_xx_34",
+    text: "认知能力的训练方法"
+}, {key: "yys_xx_35", text: "语言的训练方法"}, {key: "yys_xx_36", text: "社会行为及人格培养"}, {
+    key: "yys_xx_37",
+    text: "日常生活和初步知识教育"
+}, {key: "yys_xx_38", text: "实施个性化教学计划"}]
+}],
+    serverBMData: [{
+    key: "bm_1",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/bm/jtpr@2x.png",
+    title: "家庭烹饪",
+    subtitle: "照料全体家人的日常饮食，根据季节变化、饮食口味及时调节菜谱，做您的家庭厨师"
+}, {
+    key: "bm_2",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/bm/qjby@2x.png",
+    title: "清洁保养",
+    subtitle: "不同家具采用不用的擦具按家具要根据家具的材质控制擦具的干湿度在不同的季节对家具的通风，摆放位置，阳光照射度进行控制选择相对应的家具防护材料擦试"
+}, {
+    key: "bm_3",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/bm/czhh@2x.png",
+    title: "成长呵护",
+    subtitle: "负责孩子的日常,生活照顾，接送孩子上学督促孩子,读书，写作业"
+}, {
+    key: "bm_4",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/bm/zhlr@2x.png",
+    title: "照顾老人",
+    subtitle: "对家庭需要护理的老人，细心照料、全方位照顾老人饮食起居，以及情感陪护。给老人家人般的温暖"
+}, {
+    key: "bm_5",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/bm/kxsn@2x.png",
+    title: "科学收纳",
+    subtitle: "衣物的洗涤，保养，生活用品合理摆放、整理、科学收纳，合理利用家庭间，使家庭环境整洁、有序"
+}, {
+    key: "bm_6",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/bm/cwzl@2x.png",
+    title: "宠物的照料",
+    subtitle: "每天清洁宠物的居室对宠物居室定期进行杀菌观察宠物的心情"
+}, {
+    key: "bm_7",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/bm/jdby@2x.png",
+    title: "家电保养",
+    subtitle: "定期对家用电器采取的一系列清洗、消毒、除水垢的维护措施"
+}, {
+    key: "bm_8",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/bm/jtyy@2x.png",
+    title: "家庭园艺",
+    subtitle: "对室内，室外，屋顶等各种花卉摆放清洁，养护，栽培"
+}],
+    serverZJData: [{
+    key: "zj_1",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/zj/qmjy@2x.png",
+    title: "启蒙教育"
+}, {
+    key: "zj_2",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/zj/ggysx@2x.png",
+    title: "感官与数学"
+}, {
+    key: "zj_3",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/zj/yyyys@2x.png",
+    title: "音乐与艺术"
+}],
+    trainItemData: [{
+    key: "train_1",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/yys_2@2x.png",
+    title: p,
+    subtitle: "乳儿及幼儿的生活起居保健，辅食的添加，乳儿及幼儿常见病的预防及护理，早期教育指导及潜能开发，母乳喂养知识及喂养技巧的指导，主被动操等相关知识"
+}, {
+    key: "train_2",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/myhl_2@2x.png",
+    title: "母婴护理培训（月嫂）",
+    subtitle: "学习产妇的产前护理、产妇产后护理新生儿的护理与观察，母乳喂养知识与喂养技巧的指导等相关知识。"
+}, {
+    key: "train_3",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/crs_2@2x.png",
+    title: o,
+    subtitle: "通过跟随专业催乳师出诊观摩并实际操作，结合了中西医理论和中医点穴按摩、食疗、等方法，可迅速解决产后无乳、乳少、乳腺管堵塞、乳汁淤积、乳头凹陷、急性乳腺炎等乳房问题"
+}, {
+    key: "train_4",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/xetn_2@2x.png",
+    title: w,
+    subtitle: "小儿推拿师课程，小儿推拿生理病理特点，小儿推拿、基础知识、发育特点、治疗的作用原理及常用手法操作"
+}, {
+    key: "train_5",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/zj_2@2x.png",
+    title: d,
+    subtitle: "蒙氏早教 系统专业的做宝宝智力开发计划，行为教育，养成良好的生活习惯，进行有效的智力开发，感知能力、模仿能力、语言能力及沟通交往能力的训练。家政保姆高薪首选！"
+}, {
+    key: "train_6",
+    image: "http://cdn.xhome.net.cn/homecc-web/assets/images/train/cs_2@2x.png",
+    title: b,
+    subtitle: "各类烹饪原料的鉴别、采买、记账，特殊原料的处理与健康关系，刀工的种类、刀法、要求及作用，配菜的原则及要求，调味品的正确使用及运用知识"
+}]
+},
+    pt: {},
+    user: {token: ""}
+},
+    serverRendered: !0,
+    routePath: "/",
+    config: {}
+}
+}
+    (60, Array(1),
+    Array(6), "母婴知识", "家政问答", Array(1),
+    "13482057368", Array(1), Array(1),
+    Array(1), Array(5), Array(2),
+    "住家保姆", "高级育婴师培训", "蒙氏早教", "小儿推拿", "催乳师培训", "家庭厨师培训")
